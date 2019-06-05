550be261d74dff26585386b049907c2092355a82
r3weber <rweber@osmre.gov>
2019-06-05 09:42:14 -0600

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.1.css';
 import HomeComponent from './components/homecomponent'
 import Paintings from './components/paintingscomponent'
@@ -8,7 +8,7 @@ import About from './components/aboutcomponent'
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router >
         <p><Link className='Link' to="/home">Home</Link><Link className='Link' to="/paintings">Paintings</Link><Link className='Link' to="/about">About</Link></p>
         <div>
           <Route exact path="/paintings" component={Paintings} />
