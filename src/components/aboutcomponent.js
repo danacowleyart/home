05f3c36ea4dbef65af6280df7316193168f4c04c
r3weber <rweber@osmre.gov>
2019-06-05 08:30:07 -0600

    redirects

--- src/components/aboutcomponent.js
+++ src/components/aboutcomponent.js
@@ -5,6 +5,7 @@ const About = () => {
       <div>
         <h2>About</h2>
         <p>Artist etc</p>
+        <img src='https://www.dropbox.com/s/0on0lncl04iythj/IMG_4531.HEIC?dl=0' alt='dana'></img>
       </div>
     );
   }
