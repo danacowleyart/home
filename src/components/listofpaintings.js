303dce58606d5548e8603ae24683dd27a1efefec
r3weber <r3weber@gmail.com>
2019-06-06 19:06:08 -0600

    pictures

--- src/components/listofpaintings.js
+++ src/components/listofpaintings.js
@@ -1,34 +1,56 @@
 import kumbu from '../images/Photo Aug 25, 00 12 52.jpg'
+import happycouple from '../images/Photo Mar 30, 18 11 21.jpg'
+import happycouplethumb from '../images/Photo Mar 30, 18 11 21thumb.jpg'
+import tenmile from '../images/Photo Nov 08, 13 50 18.jpg'
+import canyon from '../images/Photo Oct 15, 23 46 46.jpg'
+import arches from '../images/Photo Oct 16, 00 11 17.jpg'
+import exodus from '../images/Photo Oct 16, 00 18 58.jpg'
 
 export const imagelist = [
     {
         id:'a',
-        name: 'Fernie',
+        name: '',
         medium: 'pastel paints',
         size: 'huge',
-        url: 'http://danacowleyart.com/landscapepaintings/images/Fernie-painting.jpg'
+        thumb: happycouplethumb,
+        url: happycouple
     },
     {
         id:'b',
         name: 'Mountains',
         medium: 'Oil on canvas',
         size: '',
-        url: 'http://danacowleyart.com/landscapepaintings/images/painting.jpg'
+        url: tenmile
     },
     {
         id:'c',
         name: 'Spider Etching',
         medium: 'pencils and crayons',
         size:'11 x 11',
-        url:'http://danacowleyart.com/prints/images/spider-etching.jpg'
+        url:canyon
     },
     {
         id:'d',
         name: 'Kumbu Valley',
         medium: 'Colored Pencil',
-        size:'14x24',
+        size:'14x36',
         url: kumbu
     },
+    {
+        id:'e',
+        name: 'Silence',
+        medium: 'Colored Pencil',
+        size:'14x24',
+        url: arches
+    },
+    {
+        id:'f',
+        name: 'Silence',
+        medium: 'Colored Pencil',
+        size:'14x24',
+        url: exodus
+    },
+    
 ];
 
 
--- src/components/paintingscomponent.js
+++ src/components/paintingscomponent.js
@@ -3,36 +3,32 @@ import imagelist from './listofpaintings'
 import ModalImage from 'react-modal-image'
 import '../App.1.css'
 
-const Paintings = () => {
-    // const myimages = []
-    // dbx.filesListFolder({path: '/photos'})
-    //   .then(response => {
-    //     response.entries.map(item => (
-    //       console.log(item)
-    //     ))
-    //     })
-      
-      
-      const List = ({ list }) => (
+class Paintings extends Component {
+  
+    render() {
+    
+
+    const List = ({ list }) => (
         <ul>
           {list.map(item => (
-            <ModalImage
+            <ModalImage style={{height:400}}
 
               small={item.url}
               large={item.url}
-              alt={<p>Name: {item.name} &nbsp;&nbsp; Medium: {item.medium} &nbsp;&nbsp; Size:</p>}
+              className= 'img'
+              alt={<p>Name: {item.name} &nbsp;&nbsp; Medium: {item.medium} &nbsp;&nbsp; Size: {item.size}</p>}
               />
           
           ))}
         </ul>
       )
+      return(
       
-  return (
-    
     
       <List list={imagelist} />
       
       
   );
+}
 }
 export default Paintings;
